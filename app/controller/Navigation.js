f9ff5e0bc22a724596f0ffee09ecc1e0a8ce23f6
Henry Munyaka <henry.munyaka@gmail.com>
2020-04-26 16:20:44 +0000

    Define the skeleton of the displayNavigatioMap method

--- app/controller/Navigation.js
+++ app/controller/Navigation.js
@@ -6,9 +6,22 @@ Ext.define('Imarisha-sys.controller.Navigation', {
   models: [ 'CitizenReport', 'IncidentClass' ],
   views: [ 'ReportsLocation' ],
 
+  navMap: null,
+
   init: function() {
     navigationController = this; //Creates a global variable to access this Controller
   },
+
+  //--- create and display the navigation map
+  displayNavigationMap: function(mapPanel){
+    if (!this.navMap) {
+      console.log('The panel ('+mapPanel.id +') is ready to deploy the map');
+      this.navMap = new OpenLayers.Map(); // temporary
+    } else {
+      console.log('navMap needs refreshing');
+    }
+  },
+  //----------------
 
   //---Controller's launch function ------------
   onLaunch: function(){
